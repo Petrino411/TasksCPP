312d6bbc544d8745e8e8afddc2c38147686352bd
Petrino411 <petr.siv@rambler.ru>
2024-09-16 17:58:30 +0300

    Update Task1_1.c

--- Task1_1/Task1_1.c
+++ Task1_1/Task1_1.c
@@ -1,37 +1,44 @@
 #include <math.h>
 #include <stdio.h>
 
+/**
+* 
+ * @param x const double
+ * @param y const double
+ * @param z const double 
+ * @return Вычисляет занчение A
+ */
+double getA(double, double,double);
+/**
+ * 
+ * @param x const double 
+ * @param y const double 
+ * @param z const double 
+ * @return Вычисляет занчение B
+ */
+double getB(double, double,double);
 
-double task1(double, double,double);
-double task2(double, double,double);
 
+/**
+ * Точка входа в программу
+ * @return 0
+ */
 int main()
 {
     const double x = 0.2;
     const double y = 0.004;
     const double z = 1.1;   
-    printf("Task1 %f \n", task1(x,y,z));
-    printf("Task2 %f \n", task2(x,y,z));
+    printf("Task1 %f \n", getA(x,y,z));
+    printf("Task2 %f \n", getB(x,y,z));
+    return 0;
 }
-/**
-* 
- * @param x 
- * @param y 
- * @param z 
- * @return double
- */
-double task1(double x,double y, double z)
+
+double getA(double x,double y, double z)
 {
     return pow( sin(pow(x,2) + pow(y,2)),3) - sqrt(x/y);
 }
-/**
- * 
- * @param x 
- * @param y 
- * @param z 
- * @return double
- */
-double task2(double x,double y, double z)
+
+double getB(double x,double y, double z)
 {
     return pow(x,2)/z + pow(cos(x+y),3);
 }
