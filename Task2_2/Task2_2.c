5e79be1568c6301f8f60498606153c4bb6b2a3d6
Petrino411 <petr.siv@rambler.ru>
2024-09-23 11:41:39 +0300

    solved tasks from part 2

--- Task2_2/Task2_2.c
+++ Task2_2/Task2_2.c
@@ -1,1 +1,47 @@
-﻿
+﻿#include <locale.h>
+#include <math.h>
+#include <stdio.h>
+
+const double a = 0.9;
+
+/**
+ * @brief Вычисляет значение системы функций
+ * @param x переменная x
+ * @return Возвращает значение y
+ */
+double get_y(double x);
+
+/**
+ * @brief Точка входа в программу
+ * @return 0
+ */
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "Rus");
+    double x = 0;
+
+    printf("Введите значение x: \n");
+    if (scanf_s("%lf", &x))
+    {
+        printf("Значение y: %lf\n", get_y(x));
+    }
+    else
+    {
+        printf("Ошибка ввода\n");
+    }
+    
+    return 0;
+}
+
+double get_y(double x)
+{
+    if (x > 1)
+    {
+        return a * log10(x) + sqrt(fabs(x));
+    }
+    if (x <= 1)
+    {
+        return 2 * a * cos(x) + 3 * pow(x, 3);
+    }
+    return 0;
+}
--- Task2_3/Task2_3.c
+++ Task2_3/Task2_3.c
@@ -1,1 +1,71 @@
-﻿
+﻿#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/**
+ * @brief Выводит приетствие соответствующее полученному из структуры времени
+ * @param input_time Структура tm с заполненными параметрами часы и минуты
+ */
+void hello_function(const struct tm* input_time);
+
+/**
+ * @brief Точка входа в программу
+ * @return 0
+ */
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "Rus");
+    struct tm time_info = {0};
+    int is_running = 1;
+    char input[10];
+
+    while (is_running)
+    {
+        printf("Введите время в формате часы:минуты (или 'q' для выхода): ");
+        scanf_s("%s", input, (unsigned)_countof(input));
+
+        if (strcmp(input, "q") == 0)
+        {
+            is_running = 0;
+            printf("Выход из программы...\n");
+            continue;
+        }
+
+        if (sscanf_s(input, "%d:%d", &time_info.tm_hour, &time_info.tm_min) != 2)
+        {
+            printf("Неправильный формат ввода.\n");
+            continue;
+        }
+
+        hello_function(&time_info);
+    }
+
+    return 0;
+}
+
+void hello_function(const struct tm* input_time)
+{
+    if (input_time->tm_hour > 23 || input_time->tm_min > 59)
+    {
+        printf("Неправильный формат ввода.\n");
+        return;
+    }
+    if (input_time->tm_hour <= 12 && input_time->tm_hour >= 6)
+    {
+        printf("Доброе утро\n");
+        return;
+    }
+    if (input_time->tm_hour > 12 && input_time->tm_hour < 17)
+    {
+        printf("Добрый день\n");
+        return;
+    }
+    if (input_time->tm_hour >= 17 && input_time->tm_hour < 23)
+    {
+        printf("Добрый вечер\n");
+        return;
+    }
+    printf("Доброй ночи\n");
+}
