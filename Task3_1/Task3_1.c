be6704db125ac48e80ce44f0777257df550df335
Petrino411 <petr.siv@rambler.ru>
2024-10-11 14:50:35 +0300

    pre

--- Task3_1/Task3_1.c
+++ Task3_1/Task3_1.c
@@ -1,6 +1,50 @@
+#include <math.h>
+#include <stdio.h>
+
+/**
+ *@brief Выводит все значения функции от 1 до 2 с шагом 0.1
+ * 
+ */
+void printTable();
+
+/**
+ * @brief Расчитывает значение функции в точке x
+ * @param x 
+ * @return double
+ */
+double getY(double x);
+
+/**
+ * @brief Факториал числа n
+ * @param n 
+ * @return факториал 
+ */
+int factorial(int n);
 
 int main(int argc, char* argv[])
 {
-    
+    printTable();
+    // printf("%d\n", factorial(10));
     return 0;
+}
+
+void printTable()
+{
+    for (double i = 0.1; i < 2.1; i += 0.1)
+    {
+        printf("%.2f: %.2f\n",i, getY(i));
+    }
+}
+
+
+double getY(double x)
+{
+    return 0.1 * pow(x, 2) - x * log10(x);
+}
+
+
+int factorial(int n)
+{
+    if (n == 0) return 1;
+    return n * factorial(n - 1);
 }
--- Task3_2/Task3_2.c
+++ Task3_2/Task3_2.c
@@ -1,6 +1,94 @@
+﻿#include <locale.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+double get_element(int k);
+
+double get_elements_sum(int n);
+
+int int_input();
+
+double double_input();
+
+long long factorial(int n);
+
+/**
+ * Выбор пользователя
+ */
+typedef enum
+{
+    TASK_A = 1,
+    TASK_B = 2
+} UserChoice;
+
 
 int main(int argc, char* argv[])
 {
-    
+    setlocale(LC_ALL, "Rus");
+    UserChoice choice = int_input();
+    switch (choice)
+    {
+    case TASK_A:
+        {
+            puts("Введите n");
+            int n = int_input();
+            double sum = get_elements_sum(n);
+            printf("Сумма первых %d элементов последовательности = %lf",n,sum);
+            break;
+        }
+    case TASK_B:
+        {
+            puts("Введите e");
+            double e = double_input();
+            break;
+            
+        }
+    }
     return 0;
+}
+
+double get_element(int k)
+{
+    return pow(-1, k - 1) / factorial(2 * k - 1);
+}
+
+double get_elements_sum(int n)
+{
+    double sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += get_element(i);
+    }
+    return sum;
+}
+
+int int_input()
+{
+    int value = 0;
+    if (scanf_s("%d", &value) != 1)
+    {
+        abort();
+    }
+    return value;
+}
+
+double double_input()
+{
+    double value = 0;
+    if (scanf_s("%lf", &value) != 1)
+    {
+        abort();
+    }
+    return value;
+}
+
+long long factorial(int n)
+{
+    long long result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
 }
