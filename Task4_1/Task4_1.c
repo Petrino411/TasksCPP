713d778913a0c9b8ed4fbd37efe9ff19fca4b0b8
Petrino411 <petr.siv@rambler.ru>
2024-10-14 15:01:21 +0300

    Update Task4_1.c

--- Task4_1/Task4_1.c
+++ Task4_1/Task4_1.c
@@ -1,119 +1,161 @@
+﻿#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX_SIZE 100  // Максимальный размер массива
 
-// Определение перечисления для выбора способа заполнения массива
-typedef enum {
-    RANDOM_FILL = 1,   // Заполнение случайными числами
-    MANUAL_FILL = 2    // Заполнение с клавиатуры
+int int_input();
+
+void print_array(int* arr, size_t size);
+
+void fill_array_random(int* arr, size_t size);
+
+void fill_array_manual(int* arr, size_t size);
+
+int sum_of_elements_less_than_10(const int* arr, size_t size);
+
+void print_indices_greater_than_next(const int* arr, size_t size);
+
+void multiply_by_third_element(int* arr, size_t size);
+
+
+typedef enum
+{
+    RANDOM_FILL = 1,
+    MANUAL_FILL = 2
 } FillMethod;
 
-// Функция для заполнения массива случайными числами в диапазоне [-40; 40]
-void fill_array_random(int *arr, int n) {
-    srand(time(0));  // Инициализация генератора случайных чисел
-    for (int i = 0; i < n; i++) {
-        *(arr + i) = (rand() % 81) - 40;  // Случайное число в диапазоне [-40, 40]
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int arr[100];
+
+
+    printf("Введите количество элементов массива (не более %d): ", 100);
+    int size = int_input();
+
+    printf("Выберите способ заполнения массива:\n");
+    printf("%d. Заполнить случайными числами\n", RANDOM_FILL);
+    printf("%d. Заполнить с клавиатуры\n", MANUAL_FILL);
+    FillMethod fill_method = int_input();
+
+    switch (fill_method)
+    {
+    case RANDOM_FILL:
+        {
+            fill_array_random(arr, size);
+            break;
+        }
+
+    case MANUAL_FILL:
+        {
+            fill_array_manual(arr, size);
+            break;
+        }
+    default:
+        {
+            printf("Неверный выбор!\n");
+            return 1;
+        }
+    }
+
+    printf("Исходный массив:\n");
+    print_array(arr, size);
+
+    int sum = sum_of_elements_less_than_10(arr, size);
+    printf("Сумма элементов, значения которых по модулю меньше 10: %d\n", sum);
+    
+    print_indices_greater_than_next(arr, size);
+    
+    multiply_by_third_element(arr, size);
+    printf("Массив после умножения элементов, кратных 3, на третий элемент:\n");
+    print_array(arr, size);
+
+    return 0;
+}
+
+int int_input()
+{
+    int value = 0;
+    if (scanf_s("%d", &value) != 1)
+    {
+        abort();
+    }
+    return value;
+}
+
+void print_array(int* arr, size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("%d ", *(arr + i));
+    }
+    putchar('\n');
+}
+
+void fill_array_random(int* arr, size_t size)
+{
+    srand(time(0));
+    for (size_t i = 0; i < size; i++)
+    {
+        *(arr + i) = rand() % 81 - 40;
     }
 }
 
-// Функция для заполнения массива с клавиатуры
-void fill_array_manual(int *arr, int n) {
-    printf("Введите %d целых чисел:\n", n);
-    for (int i = 0; i < n; i++) {
-        printf("Элемент %d: ", i + 1);
-        scanf("%d", arr + i);
+
+void fill_array_manual(int* arr, size_t size)
+{
+    printf("Введите %llu целых чисел:\n", size);
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("Элемент %llu: ", i + 1);
+        scanf_s("%d", arr + i);
     }
 }
 
-// Функция для нахождения суммы элементов по модулю меньше 10
-int sum_of_elements_less_than_10(const int *arr, int n) {
+int sum_of_elements_less_than_10(const int* arr, size_t size)
+{
     int sum = 0;
-    for (int i = 0; i < n; i++) {
-        if (abs(*(arr + i)) < 10) {
+    for (size_t i = 0; i < size; i++)
+    {
+        if (abs(*(arr + i)) < 10)
+        {
             sum += *(arr + i);
         }
     }
     return sum;
 }
 
-// Функция для вывода индексов элементов, которые больше последующего элемента
-void print_indices_greater_than_next(const int *arr, int n) {
+
+void print_indices_greater_than_next(const int* arr, size_t size)
+{
     printf("Индексы элементов, которые больше следующего:\n");
-    for (int i = 0; i < n - 1; i++) {
-        if (*(arr + i) > *(arr + i + 1)) {
-            printf("%d ", i);
+    for (size_t i = 0; i < size - 1; i++)
+    {
+        if (*(arr + i) > *(arr + i + 1))
+        {
+            printf("%llu ", i);
         }
     }
     printf("\n");
 }
 
-// Функция для умножения элементов массива, кратных 3, на третий элемент массива
-void multiply_by_third_element(int *arr, int n) {
-    if (n < 3) {
-        printf("Недостаточно элементов для умножения на третий элемент (массив должен содержать минимум 3 элемента).\n");
+void multiply_by_third_element(int* arr, size_t size)
+{
+    if (size < 3)
+    {
+        printf(
+            "Недостаточно элементов для умножения на третий элемент (массив должен содержать минимум 3 элемента).\n");
         return;
     }
-    
-    int third_element = *(arr + 2);  // Третий элемент массива
-    for (int i = 0; i < n; i++) {
-        if (*(arr + i) % 3 == 0) {
+
+    int third_element = *(arr + 2);
+    for (size_t i = 0; i < size; i++)
+    {
+        if (*(arr + i) % 3 == 0)
+        {
             *(arr + i) *= third_element;
         }
     }
-}
-
-int main() {
-    int n;
-    int arr[MAX_SIZE];
-    int choice;
-
-    // Ввод размера массива
-    printf("Введите количество элементов массива (не более %d): ", MAX_SIZE);
-    scanf("%d", &n);
-
-    // Ввод выбора метода заполнения массива через enum
-    printf("Выберите способ заполнения массива:\n");
-    printf("1. Заполнить случайными числами\n");
-    printf("2. Заполнить с клавиатуры\n");
-    scanf("%d", &choice);
-
-    FillMethod fill_method = (FillMethod)choice;  // Преобразование выбора в enum
-
-    // В зависимости от выбора вызываем соответствующую функцию
-    if (fill_method == RANDOM_FILL) {
-        fill_array_random(arr, n);
-    } else if (fill_method == MANUAL_FILL) {
-        fill_array_manual(arr, n);
-    } else {
-        printf("Неверный выбор!\n");
-        return 1;
-    }
-
-    // Вывод исходного массива
-    printf("Исходный массив:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", *(arr + i));
-    }
-    printf("\n");
-
-    // Задание 1: Найти сумму элементов, значения которых по модулю меньше 10
-    int sum = sum_of_elements_less_than_10(arr, n);
-    printf("Сумма элементов, значения которых по модулю меньше 10: %d\n", sum);
-
-    // Задание 2: Вывести индексы элементов, которые больше последующего
-    print_indices_greater_than_next(arr, n);
-
-    // Задание 3: Умножить все элементы массива, кратные 3, на третий элемент массива
-    multiply_by_third_element(arr, n);
-
-    // Вывод измененного массива
-    printf("Массив после умножения элементов, кратных 3, на третий элемент:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", *(arr + i));
-    }
-    printf("\n");
-
-    return 0;
 }
