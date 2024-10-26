36819d05a75efe4f55c62de353aceeb8a5cc9f5d
Petrino411 <petr.siv@rambler.ru>
2024-10-27 01:04:46 +0300

    fixes

--- Task4_3/Task4_3.c
+++ Task4_3/Task4_3.c
@@ -42,16 +42,19 @@ int main(void)
     printf("Массив после замены минимальных элементов каждого столбца на 0:\n");
     print_2d_array(arr_copy, n, m);
 
+    free_2d_array(arr_copy, n);
+
     // 2. Вставить после каждой строки с максимальным элементом последнюю строку.
     int** arr_copy2 = copy_2d_array(arr, n, m);
-    printf("Исходный копия:\n");
-    print_2d_array(arr_copy2, n, m);
-
-    arr_copy2 = insert_last_row_after_max(arr_copy2, &n, m);
+    
+    size_t new_n = n;
+    arr_copy2 = insert_last_row_after_max(arr_copy2, &new_n, m);
     printf("Массив после вставки последней строки:\n");
-    print_2d_array(arr_copy2, n, m);
+    print_2d_array(arr_copy2, new_n, m);
 
 
+    free_2d_array(arr_copy2, new_n);
+    
     free_2d_array(arr, n);
     return 0;
 }
@@ -128,77 +131,66 @@ void replace_min_in_columns_with_zero(int** arr, const size_t n, const size_t m)
 {
     for (size_t j = 0; j < m; j++)
     {
-        int min_index = 0;
+        int min_val = arr[0][j];
         for (size_t i = 1; i < n; i++)
         {
-            if (arr[i][j] < arr[min_index][j])
+            if (arr[i][j] < min_val)
             {
-                min_index = i;
+                min_val = arr[i][j];
             }
         }
-        arr[min_index][j] = 0;
+        for (size_t i = 0; i < n; i++)
+        {
+            if (arr[i][j] == min_val)
+            {
+                arr[i][j] = 0;
+            }
+        }
     }
 }
-
-int** insert_last_row_after_max(int** arr, size_t* n, const size_t m)
-{
-    int max_element = arr[0][0];
-
-    for (size_t i = 0; i < *n; i++)
-    {
-        for (size_t j = 0; j < m; j++)
-        {
-            if (abs(arr[i][j]) > abs(max_element))
-            {
-                max_element = arr[i][j];
+int** insert_last_row_after_max(int **arr, size_t *n, const size_t m) {
+    int max_val = arr[0][0];
+    for (size_t i = 0; i < *n; i++) {
+        for (size_t j = 0; j < m; j++) {
+            if (arr[i][j] > max_val) {
+                max_val = arr[i][j];
             }
         }
     }
-
-    size_t original_n = *n;
-    for (size_t i = 0; i < original_n; i++)
-    {
-        int contains_max = 0;
-        for (size_t j = 0; j < m; j++)
-        {
-            if (abs(arr[i][j]) == abs(max_element))
-            {
-                contains_max = 1;
+    
+    const int *last_row = arr[*n - 1];
+    
+    for (size_t i = 0; i < *n; i++) {
+        int has_max = 0;
+        for (size_t j = 0; j < m; j++) {
+            if (arr[i][j] == max_val) {
+                has_max = 1;
                 break;
             }
         }
-
-        if (contains_max)
-        {
-            arr = realloc(arr, (*n + 1) * sizeof(int*));
-
-
-            arr[*n] = (int*)malloc(m * sizeof(int));
-
-
-            for (size_t j = 0; j < m; j++)
-            {
-                arr[*n][j] = arr[original_n - 1][j];
+        if (has_max) {
+            (*n)++;
+            arr = realloc(arr, *n * sizeof(int*));
+            if (!arr) {
+                puts("Ошибка при выделении памяти.");
+                abort();
             }
-
-            for (size_t k = *n; k > i + 1; k--)
-            {
+            
+            for (size_t k = *n - 1; k > i + 1; k--) {
                 arr[k] = arr[k - 1];
             }
-
-            arr[i + 1] = (int*)malloc(m * sizeof(int));
-            for (size_t j = 0; j < m; j++)
-            {
-                arr[i + 1][j] = arr[*n][j];
+            
+            arr[i + 1] = malloc(m * sizeof(int));
+            for (size_t j = 0; j < m; j++) {
+                arr[i + 1][j] = last_row[j];
             }
 
-            (*n)++;
-            i++;
+            i++; 
         }
     }
-
     return arr;
 }
+
 
 void free_2d_array(int** arr, const size_t n)
 {
