152aaa23cf5ef68e3c7b9093c227fa352ee4da23
Пётр Сивунов <petr.siv@rambler.ru>
2025-05-13 01:55:11 +0300

    Update main.cpp

--- OOP_C++/Task_2/main.cpp
+++ OOP_C++/Task_2/main.cpp
@@ -3,41 +3,17 @@
 #include "Point.h"
 #include <limits>
 
+/**
+ * @brief Функция для ввода точки.
+ * @return Точка, введённая пользователем.
+ */
+Point inputPoint();
 
-Point inputPoint() {
-    double x, y, z;
-    std::cout << "Введите координаты точки (x y z): ";
-    while (!(std::cin >> x >> y >> z)) {
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout << "Ошибка ввода. Повторите ввод: ";
-    }
-    return {x, y, z};
-}
-
-Cylinder inputCylinder() {
-    std::cout << "Введите данные для цилиндра:\n";
-    auto p = inputPoint();
-    double radius, height;
-
-
-    std::cout << "Введите радиус цилиндра: ";
-    while (!(std::cin >> radius) || radius <= 0) {
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout << "Ошибка: радиус должен быть положительным числом. Повторите ввод: ";
-    }
-
-    std::cout << "Введите высоту цилиндра: ";
-    while (!(std::cin >> height) || height <= 0) {
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cout << "Ошибка: высота должна быть положительным числом. Повторите ввод: ";
-    }
-
-    return Cylinder(p, radius, height);
-}
-
+/**
+ * @brief Функция для ввода цилиндра.
+ * @return Цилиндр, введённый пользователем.
+ */
+Cylinder inputCylinder();
 
 /**
 * @brief Главная функция программы.
@@ -63,5 +39,41 @@ int main(int argc, char *argv[]) {
 
 }
 
+
+Point inputPoint() {
+    double x = 0, y = 0, z = 0;
+    std::cout << "Введите координаты точки (x y z): ";
+    while (!(std::cin >> x >> y >> z)) {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Ошибка ввода. Повторите ввод: ";
+    }
+    return {x, y, z};
+
+
+}
+
+Cylinder inputCylinder() {
+    std::cout << "Введите данные для цилиндра:\n";
+    auto p = inputPoint();
+    double radius = 0, height = 0;
+
+
+    std::cout << "Введите радиус цилиндра: ";
+    while (!(std::cin >> radius) || radius <= 0) {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Ошибка: радиус должен быть положительным числом. Повторите ввод: ";
+    }
+
+    std::cout << "Введите высоту цилиндра: ";
+    while (!(std::cin >> height) || height <= 0) {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Ошибка: высота должна быть положительным числом. Повторите ввод: ";
+    }
+
+    return Cylinder(p, radius, height);
+}
 
 
